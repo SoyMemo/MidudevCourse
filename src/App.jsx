ce1d497ec9f988bd6ad601f9621d754fc20265e2
SoyMemoo <merbisbaez@gmail.com>
2023-06-16 16:31:12 -0500

    Se crearon ficheros para separar componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,10 @@
 
 // import './App.css'
-
+import confetti from "canvas-confetti"
 import { useState } from "react"
+import { Square } from "./components/Square"
+import { TURNS, WINNER_COMBOS } from "./constants"
 
-const TURNS = {
-  X: 'X',
-  O: 'O'
-}
-
-
-const Square =({children, updateBoard, isSelected, index}) => {
-  const className = `square ${isSelected ? 'is-selected' : ' '}`
-  const handleClick = () => {
-    updateBoard(index)
-  } 
-  return (
-  <div onClick={handleClick} className={className}>
-    {children}
-  </div>
-)
-}
-
-const WINNER_COMBOS = [
-  [ 0, 1, 2 ],
-  [ 3, 4, 5 ],
-  [ 6, 7, 8 ],
-  [ 0, 3, 6 ],
-  [ 1, 4, 7 ],
-  [ 2, 5, 8 ],
-  [ 0, 4, 8 ] ,
-  [ 2, 4, 6 ]
-]
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
@@ -72,6 +46,7 @@ function App() {
     setTurn(newTurn)
     const newWinner = checkWinner(newBoard)
     if(newWinner){
+      confetti()
       setWinner(newWinner)
     } else if(checkEndGame(newBoard)) {
       setWinner(false)
